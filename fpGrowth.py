dc7a7dd59a23f96f662699a2fd17adcf78848c76
Xhadow0823_NB <44898836+Xhadow0823@users.noreply.github.com>
2019-12-10 17:31:56 +0800

    add comment in fpGrowth.py
    as title

--- fpGrowth.py
+++ fpGrowth.py
@@ -20,33 +20,35 @@ class treeNode:
             child.disp(ind + 1)
 
 #構建FP-tree
-def createTree(dataSet, minSup=1):
+def createTree(dataSet, minSup=1):  #parm dataset 是 {frozenset(items) : 數量}
     headerTable = {}
-    for trans in dataSet:  #第一次遍歷：統計各個數據的頻繁度
+    for trans in dataSet:  #第一次遍歷：統計各個數據的頻繁度  #做出headerTable  #trans是frozenset( item )
         for item in trans:
-            headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
+            headerTable[item] = headerTable.get(item, 0) + dataSet[trans]  #一開始是存數字 #dataSet[trans]代表item的數量
             #用頭指針表統計各個類別的出現的次數，計算頻繁量：頭指針表[類別]=出現次數
     for k in list(headerTable):  #刪除未達到最小頻繁度的數據
         if headerTable[k] < minSup:
             del (headerTable[k])
-    freqItemSet = set(headerTable.keys())#保存達到要求的數據
+    freqItemSet = set(headerTable.keys())#保存達到要求的數據  # freqItemSet是所有符合支持度的item集合
     # print ('freqItemSet: ',freqItemSet)
     if len(freqItemSet) == 0:
         return None, None  #若達到要求的數目為0
     for k in headerTable: #遍歷頭指針表
-        headerTable[k] = [headerTable[k], None]  #保存計數值及指向每種類型第一個元素項的指針
+        headerTable[k] = [headerTable[k], None]  #保存計數值及指向每種類型第一個元素項的指針 # value=[數量, ptr]
     # print ('headerTable: ',headerTable)
+
     retTree = treeNode('Null Set', 1, None)  #初始化tree
-    for tranSet, count in dataSet.items():  # 第二次遍歷：
+    for tranSet, count in dataSet.items():  # 第二次遍歷： #構成FP-tree
         localD = {}
         for item in tranSet:  # put transaction items in order
             if item in freqItemSet:#只對頻繁項集進行排序
-                localD[item] = headerTable[item][0]
+                localD[item] = headerTable[item][0]  #localD[item]=數量
 
         #使用排序後的頻率項集對樹進行填充
         if len(localD) > 0:
-            orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
+            orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]  #  WATCHOUT!!!
             updateTree(orderedItems, retTree, headerTable, count)  # populate tree with ordered freq itemset
+            #         (items,        inTree,  headerTable, count)
     return retTree, headerTable  #返回樹和頭指針表
 
 
@@ -78,12 +80,13 @@ def loadSimpDat():
                ['y', 'r', 'x', 'z', 'q', 't', 'p'],
                ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
     return simpDat
+
 #createInitSet() 用於實現上述從列表到字典的類型轉換過程
-def createInitSet(dataSet):
+def createInitSet(dataSet):  #parm dataset is list[list[]]
     retDict = {}
     for trans in dataSet:
         retDict[frozenset(trans)] = 1
-    return retDict
+    return retDict  # retDict is {frozenset() : 1}
 
 #從FP樹中發現頻繁項集
 def ascendTree(leafNode, prefixPath):  #遞歸上溯整棵樹
@@ -114,14 +117,14 @@ def mineTree(inTree, headerTable, minSup, preFix, freqItemList):
         freqItemList.append(newFreqSet)
         #遞歸調用函數來創建基
         condPattBases = findPrefixPath(basePat, headerTable[basePat][1])
-        #print ('condPattBases :',basePat, condPattBases)
+        print ('condPattBases :',basePat, condPattBases)
 
         #2. 構建條件模式Tree
         myCondTree, myHead = createTree(condPattBases, minSup)
         #將創建的條件基作為新的數據集添加到fp-tree
-        #print ('head from conditional tree: ', myHead)
+        print ('head from conditional tree: ', myHead)
         if myHead != None: #3. 遞歸
-            # print ('conditional tree for: ',newFreqSet)
-            # myCondTree.disp(1)
+            print ('conditional tree for: ',newFreqSet)
+            myCondTree.disp(1)
             mineTree(myCondTree, myHead, minSup, newFreqSet, freqItemList)
 
